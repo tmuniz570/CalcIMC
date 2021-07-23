f1b8add49985dd60b65f732a63a3983632e73888
tmuniz570 <tmuniz570@gmail.com>
2021-07-23 05:38:57 -0300

    Icluiu peso max e min ideal para a altura de acordo com o IMC.

--- app/src/main/java/com/tmuniz570/calcimc/MainActivity.kt
+++ app/src/main/java/com/tmuniz570/calcimc/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +26,25 @@ class MainActivity : AppCompatActivity() {
         val altura = et_altura.text.toString().toFloatOrNull()
 
         if (peso != null && altura != null){
-            val imc = peso / (altura * altura)
+            val imc = peso / altura.pow(2)
+            val pesoMin = 18.5 * altura.pow(2)
+            val pesoMax = 24.9 * altura.pow(2)
 
             when{
                 imc < 18.5 -> {
-                    tv_result.text = "Seu IMC é: %.2f\n\nClassificado como MAGREZA\n\nGrau de obesidade: 0".format(imc)
+                    tv_result.text = "Seu IMC é: %.2f Kg/m²\n\nClassificado como MAGREZA\n\nGrau de obesidade: 0\n\nSeu peso ideal seria entre %.2f Kg e %.2f Kg".format(imc, pesoMin, pesoMax)
                 }
                 imc >= 18.5 && imc < 25 -> {
-                    tv_result.text = "Seu IMC é: %.2f\n\nClassificado como NORMAL\n\nGrau de obesidade: 0".format(imc)
+                    tv_result.text = "Seu IMC é: %.2f Kg/m²\n\nClassificado como NORMAL\n\nGrau de obesidade: 0\n\nSeu peso ideal é entre %.2f Kg e %.2f Kg".format(imc, pesoMin, pesoMax)
                 }
                 imc >= 25 && imc < 30 -> {
-                    tv_result.text = "Seu IMC é: %.2f\n\nClassificado como SOBREPESO\n\nGrau de obesidade: 1".format(imc)
+                    tv_result.text = "Seu IMC é: %.2f Kg/m²\n\nClassificado como SOBREPESO\n\nGrau de obesidade: 1\n\nSeu peso ideal seria entre %.2f Kg e %.2f Kg".format(imc, pesoMin, pesoMax)
                 }
                 imc >= 30 && imc < 40 -> {
-                    tv_result.text = "Seu IMC é: %.2f\n\nClassificado como OBESIDADE\n\nGrau de obesidade: 2".format(imc)
+                    tv_result.text = "Seu IMC é: %.2f Kg/m²\n\nClassificado como OBESIDADE\n\nGrau de obesidade: 2\n\nSeu peso ideal seria entre %.2f Kg e %.2f Kg".format(imc, pesoMin, pesoMax)
                 }
                 imc >= 40 -> {
-                    tv_result.text = "Seu IMC é: %.2f\n\nClassificado como OBESIDADE GRAVE\t\n\nGrau de obesidade: 3".format(imc)
+                    tv_result.text = "Seu IMC é: %.2f Kg/m²\n\nClassificado como OBESIDADE GRAVE\n\nGrau de obesidade: 3\n\nSeu peso ideal seria entre %.2f Kg e %.2f Kg".format(imc, pesoMin, pesoMax)
                 }
             }
             tv_result.visibility = View.VISIBLE
